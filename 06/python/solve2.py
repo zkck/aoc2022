17cc57566fc7d994440a4d06299253c492362866
Zak Cook <zakcook4@gmail.com>
2022-12-06 11:12:54 +0100

    Change to cleaner class structure for day 6

--- 06/python/solve2.py
+++ 06/python/solve2.py
@@ -4,22 +4,48 @@ from collections import Counter
 COUNT = 14
 
 
+def _get_ord(char):
+    return ord(char) - ord("a")
+
+
+class CharCounter:
+    def __init__(self, iterable) -> None:
+        self.counts = [0 for _ in range(26)]
+        self._num_nonzero = 0
+        for char in iterable:
+            self.inc(char)
+
+    @property
+    def num_nonzero(self):
+        return self._num_nonzero
+
+    def inc(self, char):
+        idx = _get_ord(char)
+        if self.counts[idx] == 0:
+            self._num_nonzero += 1
+        self.counts[idx] += 1
+
+    def dec(self, char):
+        idx = _get_ord(char)
+        self.counts[idx] -= 1
+        if self.counts[idx] == 0:
+            self._num_nonzero -= 1
+
+
 def solve(line):
     i = COUNT - 1
 
-    seen = Counter(line[:i])
+    seen = CharCounter(line[:i])
     while i < len(line):
         print(seen)
         # Add current
-        seen[line[i]] += 1
+        seen.inc(line[i])
         # Remove
-        if len(seen) == COUNT:
+        if seen.num_nonzero == COUNT:
             return i + 1
 
         back = i - COUNT + 1
-        seen[line[back]] -= 1
-        if seen[line[back]] == 0:
-            del seen[line[back]]
+        seen.dec(line[back])
 
         i += 1
 
