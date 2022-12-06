e58b367ca58250cbd1657cb6d51b3e69f764a3ca
Zak Cook <zakcook4@gmail.com>
2022-12-06 11:24:50 +0100

    Generic solution for day 2

---
+++ 06/python/solve.py
@@ -0,0 +1,56 @@
+import sys
+
+
+def _get_ord(char):
+    return ord(char) - ord("a")
+
+
+class CharCounter:
+    def __init__(self, iterable) -> None:
+        self._counts = [0 for _ in range(26)]
+        self._num_nonzero = 0
+        for char in iterable:
+            self.inc(char)
+
+    @property
+    def num_nonzero(self):
+        return self._num_nonzero
+
+    def inc(self, char):
+        idx = _get_ord(char)
+        if self._counts[idx] == 0:
+            self._num_nonzero += 1
+        self._counts[idx] += 1
+
+    def dec(self, char):
+        idx = _get_ord(char)
+        self._counts[idx] -= 1
+        if self._counts[idx] == 0:
+            self._num_nonzero -= 1
+
+
+def solve(line, count):
+    i = count - 1
+
+    seen = CharCounter(line[:i])
+    while i < len(line):
+        print(seen)
+        # Add current
+        seen.inc(line[i])
+        # Remove
+        if seen.num_nonzero == count:
+            return i + 1
+
+        back = i - count + 1
+        seen.dec(line[back])
+
+        i += 1
+
+
+def main():
+    (count,) = sys.argv[1:]
+    print(solve(sys.stdin.readline().rstrip("\n"), int(count)))
+
+
+if __name__ == "__main__":
+    main()
