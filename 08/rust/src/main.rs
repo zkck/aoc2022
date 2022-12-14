056a86cf90e17b15675fef9a33829225babeeb89
Zak Cook <zakcook4@gmail.com>
2022-12-14 08:16:13 +0100

    Solve day 8 part 2 in Rust

--- 08/rust/src/main.rs
+++ 08/rust/src/main.rs
@@ -81,11 +81,26 @@ fn mark_visible_trees<T>(
     }
 }
 
-fn main() {
-    let lines: Vec<Vec<char>> = io::stdin()
-        .lines()
-        .map(|l| l.unwrap().chars().collect())
-        .collect();
+fn mark_scenic_ranges<T>(
+    indices_matrix: &T,
+    lines: &Vec<Vec<char>>,
+    scenic_ranges: &mut Vec<Vec<u32>>,
+) where
+    T: Matrix<(usize, usize)>,
+{
+    for i in 0..indices_matrix.height() {
+        let mut seen: Vec<u32> = vec![];
+        for j in 0..indices_matrix.width() {
+            let (i, j) = indices_matrix.value_at(i, j);
+            let tree_height = lines[i][j].to_digit(10).unwrap() as u32;
+            let blocking_tree_position = seen.iter().rposition(|&h| h >= tree_height).unwrap_or(0);
+            scenic_ranges[i][j] *= (seen.len() - blocking_tree_position) as u32;
+            seen.push(tree_height);
+        }
+    }
+}
+
+fn solve1(lines: &Vec<Vec<char>>) -> u32 {
     let (width, height) = (lines.len(), lines[0].len());
     let mut indices_matrix = NRotatedMatrix {
         base_matrix: IndicesMatrix { width, height },
@@ -94,8 +109,30 @@ fn main() {
     let mut visible_trees = vec![vec![false; width]; height];
     for _ in 0..4 {
         mark_visible_trees(&indices_matrix, &lines, &mut visible_trees);
-        indices_matrix.n += 1
+        indices_matrix.n += 1;
     }
-    let count: u32 = visible_trees.iter().flatten().map(|&b| b as u32).sum();
-    println!("{}", count);
+    visible_trees.iter().flatten().map(|&b| b as u32).sum()
+}
+
+fn solve2(lines: &Vec<Vec<char>>) -> u32 {
+    let (width, height) = (lines.len(), lines[0].len());
+    let mut indices_matrix = NRotatedMatrix {
+        base_matrix: IndicesMatrix { width, height },
+        n: 0,
+    };
+    let mut scenic_ranges = vec![vec![1; width]; height];
+    for _ in 0..4 {
+        mark_scenic_ranges(&indices_matrix, &lines, &mut scenic_ranges);
+        indices_matrix.n += 1;
+    }
+    *scenic_ranges.iter().flatten().max().unwrap()
+}
+
+fn main() {
+    let lines: Vec<Vec<char>> = io::stdin()
+        .lines()
+        .map(|l| l.unwrap().chars().collect())
+        .collect();
+    println!("ans1 {}", solve1(&lines));
+    println!("ans2 {}", solve2(&lines));
 }
