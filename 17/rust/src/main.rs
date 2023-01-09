9e9ba956e5a3ea1d24a1333e80bb64d68d3b6fb8
Zak Cook <zakcook4@gmail.com>
2023-01-09 08:08:24 +0100

    Solve day 17 part 1

--- 17/rust/src/main.rs
+++ 17/rust/src/main.rs
@@ -1,4 +1,27 @@
-use std::{fmt::Display, io::{self, BufReader, BufRead}, fs::File};
+use std::fmt::Display;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+
+const INPUT_FILEPATH: &str = "../input";
+const HEAD: usize = 10;
+
+enum Action {
+    Forward,
+    Backward,
+}
+
+impl TryFrom<char> for Action {
+    type Error = &'static str;
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            '<' => Ok(Self::Backward),
+            '>' => Ok(Self::Forward),
+            _ => Err("Invalid character."),
+        }
+    }
+}
 
 fn rocks() -> Vec<Vec<Vec<bool>>> {
     vec![
@@ -58,7 +81,7 @@ impl Well {
             }
             for (dj, &is_solid) in row.iter().enumerate() {
                 let j = j + dj;
-                self.well[i][j] = is_solid;
+                self.well[i][j] |= is_solid;
             }
         }
     }
@@ -66,7 +89,7 @@ impl Well {
 
 impl Display for Well {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in self.well.iter().rev() {
+        for row in self.well.iter().rev().take(HEAD) {
             writeln!(
                 f,
                 "{}",
@@ -74,20 +97,20 @@ impl Display for Well {
                     .join("")
             )?;
         }
-        writeln!(f)
+        Ok(())
     }
 }
 
-fn drop<T>(rock: &Vec<Vec<bool>>, well: &mut Well, actions: &mut T)
+fn drop<I>(rock: &Vec<Vec<bool>>, well: &mut Well, actions: &mut I)
 where
-    T: Iterator<Item = char>,
+    I: Iterator<Item = Action>,
 {
     let (mut i, mut j) = (well.well.len() as i32 + 3, 2);
     loop {
         let dj = match actions.next() {
-            Some('<') => -1,
-            Some('>') => 1,
-            _ => panic!("Unexpected character or EOL"),
+            Some(Action::Forward) => 1,
+            Some(Action::Backward) => -1,
+            None => panic!("EOF reached."),
         };
         if !well.conflicts(rock, (i, j + dj)) {
             j += dj;
@@ -98,26 +121,37 @@ where
         i -= 1;
     }
     well.solidify(rock, (i as usize, j as usize));
+    println!("{}", well);
 }
 
 fn solve<T>(actions: &mut T) -> usize
 where
-    T: Iterator<Item = char>,
+    T: Iterator<Item = Action>,
 {
     let rocks = rocks();
     let mut well = Well { well: vec![] };
-    for i in 0..2022 {
-        drop(&rocks[i % rocks.len()], &mut well, actions);
-    }
+    rocks.iter().cycle().take(2022).for_each(|rock| {
+        drop(rock, &mut well, actions);
+    });
+    println!("{}", well);
     well.well.len()
 }
 
 fn main() -> io::Result<()> {
-    let file = File::open("../input")?;
+    let file = File::open(INPUT_FILEPATH)?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        println!("ans1 {}", solve(&mut line?.chars().cycle()));
+        println!(
+            "ans1 {}",
+            solve(
+                &mut line?
+                    .chars()
+                    .map(Action::try_from)
+                    .map(Result::unwrap)
+                    .cycle()
+            )
+        );
         return Ok(());
     }
     panic!("File is empty")
