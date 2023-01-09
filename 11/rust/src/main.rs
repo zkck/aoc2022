b2bbbdc1a79d7b2b0135dc602b87c99935a8b1e9
Zak Cook <zakcook4@gmail.com>
2023-01-09 07:33:26 +0100

    Solve day 11 part 2

--- 11/rust/src/main.rs
+++ 11/rust/src/main.rs
@@ -2,10 +2,21 @@ use std::collections::HashMap;
 use std::fs;
 use std::str::SplitWhitespace;
 
+const NUM_ROUNDS: usize = 10000;
+const INPUT_FILEPATH: &str = "../test";
+
+const MONKEY_REGEX: &str = r"Monkey (\d+):
+  Starting items: (.+)
+  Operation: new = (.+)
+  Test: divisible by (\d+)
+    If true: throw to monkey (\d)
+    If false: throw to monkey (\d)
+";
+
 #[derive(Debug)]
 enum Expression {
     Identifier(String),
-    Constant(i32),
+    Constant(u64),
     Addition(Box<Expression>, Box<Expression>),
     Multiplication(Box<Expression>, Box<Expression>),
 }
@@ -35,29 +46,33 @@ impl Expression {
         }
     }
 
-    fn evaluate(&self, context: &HashMap<String, i32>) -> i32 {
+    fn evaluate(&self, context: &HashMap<String, u64>, modulo: u64) -> u64 {
         match self {
             Expression::Identifier(ident) => context[ident],
             Expression::Constant(i) => *i,
-            Expression::Addition(e1, e2) => e1.evaluate(context) + e2.evaluate(context),
-            Expression::Multiplication(e1, e2) => e1.evaluate(context) * e2.evaluate(context),
+            Expression::Addition(e1, e2) => e1.evaluate(context, modulo) + e2.evaluate(context, modulo),
+            Expression::Multiplication(e1, e2) => {
+                let a = e1.evaluate(context, modulo);
+                let b = e2.evaluate(context, modulo);
+                a * b % modulo
+            },
         }
     }
 }
 
 trait Test {
-    fn get_monkey(&self, worry_level: i32) -> usize;
+    fn get_monkey(&self, worry_level: u64) -> usize;
 }
 
 #[derive(Debug)]
 struct DivisibilityTest {
-    divider: i32,
+    divider: u64,
     monkey_id_true: usize,
     monkey_id_false: usize,
 }
 
 impl Test for DivisibilityTest {
-    fn get_monkey(&self, worry_level: i32) -> usize {
+    fn get_monkey(&self, worry_level: u64) -> usize {
         if worry_level % self.divider == 0 {
             self.monkey_id_true
         } else {
@@ -66,28 +81,16 @@ impl Test for DivisibilityTest {
     }
 }
 
-trait Operation {
-    fn change_worry_level();
-}
-
 #[derive(Debug)]
 struct Monkey {
     operation: Expression,
     test: DivisibilityTest,
 }
 
-const NUM_ROUNDS: usize = 20;
-
-const MONKEY_REGEX: &str = r"Monkey (\d+):
-  Starting items: (.+)
-  Operation: new = (.+)
-  Test: divisible by (\d+)
-    If true: throw to monkey (\d)
-    If false: throw to monkey (\d)
-";
-
-fn run(monkeys_by_id: &Vec<Monkey>, items_by_monkey: &mut HashMap<usize, Vec<i32>>) {
+fn run(monkeys_by_id: &Vec<Monkey>, items_by_monkey: &mut HashMap<usize, Vec<u64>>) {
     let mut inspection_counts: Vec<usize> = vec![0; items_by_monkey.len()];
+    // all values in the data are primes, so no need to do find lcm
+    let modulo = monkeys_by_id.iter().map(|m| m.test.divider).fold(1, |acc, x| acc * x);
 
     for _round in 0..NUM_ROUNDS {
         for (monkey_id, monkey) in monkeys_by_id.iter().enumerate() {
@@ -102,32 +105,28 @@ fn run(monkeys_by_id: &Vec<Monkey>, items_by_monkey: &mut HashMap<usize, Vec<i32
                 let mut context = HashMap::new();
                 context.insert("old".to_string(), item_worry_level);
 
-                item_worry_level = monkey.operation.evaluate(&context);
-                item_worry_level /= 3;
+                item_worry_level = monkey.operation.evaluate(&context, modulo);
 
                 let to_monkey_id = monkey.test.get_monkey(item_worry_level);
                 let entry = items_by_monkey.entry(to_monkey_id);
                 entry.or_insert(vec![]).push(item_worry_level);
             }
         }
-
-        println!("{:?}", items_by_monkey);
     }
 
     inspection_counts.sort();
     inspection_counts.reverse();
 
-    println!("{:?}", inspection_counts);
-    println!("ans1 {:?}", inspection_counts.iter().take(2).fold(1, |a, &b| {a * b}));
+    println!("ans {:?}", inspection_counts.iter().take(2).fold(1, |a, &b| {a * b}));
 }
 
 fn main() {
     let monkey_regex = regex::Regex::new(MONKEY_REGEX).unwrap();
 
-    let input = fs::read_to_string("../input").unwrap();
+    let input = fs::read_to_string(INPUT_FILEPATH).unwrap();
     let mut monkeys: Vec<Monkey> = vec![];
 
-    let mut items_by_monkey: HashMap<usize, Vec<i32>> = HashMap::new();
+    let mut items_by_monkey: HashMap<usize, Vec<u64>> = HashMap::new();
 
     for capture in monkey_regex.captures_iter(&input) {
         let group = |idx| capture.get(idx).unwrap().as_str();
